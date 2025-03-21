28b99f3beab81a6afe240f6aa947bd0a460f5896
SamM <s.mcdonald@outlook.com.au>
2025-03-21 20:46:28 +1000

    added: Entry.cpp file

---
+++ src/Entry.cpp
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+ // C & C++ Libs
+#include <iostream>
+
+// OpenGl Libs
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello World. \n";
+}
