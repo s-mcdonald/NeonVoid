4ab0d76bd2dfed45072d9dea33f9691e3539bb5a
SamM <s.mcdonald@outlook.com.au>
2025-03-27 22:23:39 +1000

    build: updated vs build scripts

--- scripts/setup-vs-2019.bat
+++ scripts/setup-vs-2019.bat
@@ -3,7 +3,7 @@
 :: Set the build configuration (Debug or Release)
 set BUILD_TYPE=Debug
 
-cmake -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% .
+cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% .
 
 cmake --build . --config %BUILD_TYPE%
 
