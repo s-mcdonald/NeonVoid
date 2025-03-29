930f18d8b6a6ae1aeed343763504c12757fd0eca
SamM <s.mcdonald@outlook.com.au>
2025-03-29 13:03:15 +1000

    comment: note to self

--- src/Game/src/GameMain.cpp
+++ src/Game/src/GameMain.cpp
@@ -16,10 +16,14 @@
  
      Game game;
      GameEngine gameEngine;
+
+        // @todo: I think the gameEngine should be injected into the game, 
+        //          so that its what runbs the game, and does not need to be defined 
+        //          in the main entry point, the game manages the engine.
  
      LoadGameData(&game, gameEngine);
  
-     if (gameEngine.Initialize(800, 600, "Neon Void (0.0.1)")) 
+     if (gameEngine.Initialize(800, 600, "Neon Void (0.0.2)")) 
      {
          gameEngine.Run(&game);
      }
