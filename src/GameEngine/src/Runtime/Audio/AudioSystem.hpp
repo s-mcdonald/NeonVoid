6c447d8bb42ec50dabe0be34a926f07718c5f205
SamM <s.mcdonald@outlook.com.au>
2025-03-29 19:20:19 +1000

    refactor: AudioSystem now has own hpp

---
+++ src/GameEngine/src/Runtime/Audio/AudioSystem.hpp
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+#pragma once
+
+#include <miniaudio.h>
+
+#include "Volume.hpp"
+
+namespace Neon 
+{
+    class AudioSystem 
+    {
+        public:
+            AudioSystem();
+            ~AudioSystem();
+
+        public:
+            virtual void Play(const std::string& filename);
+            virtual void PlayOnce(const std::string& filename);
+            virtual void PlayOnLoop(const std::string& filename);
+            virtual void Stop();
+            virtual void Update();
+
+            void SetVolume(const Volume& volume);
+
+        private:
+            ma_device_config getDeviceConfig();
+            static void dataCallback(ma_device* pDevice, void* pOutput, const void* pInput, uint32_t frameCount);
+
+        private:
+            ma_decoder m_decoder;
+            ma_device m_device;
+            bool m_isPlaying{false};
+            bool m_onLoop{false};
+            bool m_isStopRequested{false};
+    };
+}
--- src/GameEngine/src/Runtime/Runtime.hpp
+++ src/GameEngine/src/Runtime/Runtime.hpp
@@ -16,38 +16,13 @@
 #include <miniaudio.h>
 
 #include "Types.hpp"
+
+#include "Audio/AudioSystem.hpp"
 #include "Audio/Volume.hpp"
 
 namespace Neon 
 {
     class Game;
-
-    class AudioSystem 
-    {
-        public:
-            AudioSystem();
-            ~AudioSystem();
-
-        public:
-            virtual void Play(const std::string& filename);
-            virtual void PlayOnce(const std::string& filename);
-            virtual void PlayOnLoop(const std::string& filename);
-            virtual void Stop();
-            virtual void Update();
-
-            void SetVolume(const Volume& volume);
-
-        private:
-            ma_device_config getDeviceConfig();
-            static void dataCallback(ma_device* pDevice, void* pOutput, const void* pInput, uint32_t frameCount);
-
-        private:
-            ma_decoder m_decoder;
-            ma_device m_device;
-            bool m_isPlaying{false};
-            bool m_onLoop{false};
-            bool m_isStopRequested{false};
-    };
 
     using InitFunction = std::function<void()>;
     using RenderFunction = std::function<void()>;
