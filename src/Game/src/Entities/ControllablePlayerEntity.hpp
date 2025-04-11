868c2b3dcbce313fb6d78a5e94a65508e921fa39
SamM <s.mcdonald@outlook.com.au>
2025-04-12 09:27:56 +1000

    feat: add Mankind header file

--- src/Game/src/Entities/ControllablePlayerEntity.hpp
+++ src/Game/src/Entities/ControllablePlayerEntity.hpp
@@ -2,15 +2,16 @@
 #pragma once
 
 #include <Runtime/Entity/Entity.hpp>
+#include "../Mankind.hpp"
 
 namespace Neon
 {
     class ControllablePlayerEntity final : public Entity
     {
         public:
-            ControllablePlayerEntity(): Entity(1) {};
+            ControllablePlayerEntity(): Entity(ENTITY_ID_MAIN_PLAYER) {};
             ControllablePlayerEntity(const std::initializer_list<Component*> components)
-                : Entity(1, components) {};
+                : Entity(ENTITY_ID_MAIN_PLAYER, components) {};
 
             ~ControllablePlayerEntity() override = default;
 
---
+++ src/Game/src/Mankind.hpp
@@ -0,0 +1,8 @@
+
+#pragma once
+
+
+const int MANKIND_VERSION = 0;
+
+const uint32_t ENTITY_ID_MAIN_PLAYER = 1;
+const uint32_t ENTITY_ID_LEVEL_1_BOSS = 10;
