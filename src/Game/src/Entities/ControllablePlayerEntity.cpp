aed831164155f8068e10d297546f3f1362331c89
SamM <s.mcdonald@outlook.com.au>
2025-04-12 10:38:21 +1000

    added: Component to game entity

--- src/Game/src/Entities/ControllablePlayerEntity.cpp
+++ src/Game/src/Entities/ControllablePlayerEntity.cpp
@@ -1,20 +1,43 @@
 
+#include <iostream>
+
 #include "ControllablePlayerEntity.hpp"
 
 namespace Neon
 {
     void ControllablePlayerEntity::OnInit()
     {
+        std::cout << "ControllablePlayerEntity::OnInit()" << std::endl;
 
+        ///
+        /// Green Triangle
+        ///
+        std::vector<float> vertices = {
+            0.0f,  0.5f,   // Top vertex
+           -0.5f, -0.5f,   // Bottom left vertex
+            0.5f, -0.5f    // Bottom right vertex
+        };
+
+        auto shader = new Shader(
+            "/home/sam/Game/Neon/src/Game/assets/shaders/GreenTriangle/shader.vert",
+            "/home/sam/Game/Neon/src/Game/assets/shaders/GreenTriangle/shader.frag"
+        );
+
+        auto* component = new ShaderComponent(vertices, shader);
+        AddComponent(component);
     }
 
     void ControllablePlayerEntity::OnUpdate()
-    {
-
+    {;
+        #if defined(NEON_DEBUG) && defined(NEON_DEBUG_VERBOSE)
+                std::cout << "ControllablePlayerEntity::OnUpdate()" << std::endl;
+        #endif
     }
 
     void ControllablePlayerEntity::OnDestroy()
     {
-
+#if defined(NEON_DEBUG) && defined(NEON_DEBUG_VERBOSE)
+        std::cout << "ControllablePlayerEntity::OnDestroy()" << std::endl;
+#endif
     }
 }
