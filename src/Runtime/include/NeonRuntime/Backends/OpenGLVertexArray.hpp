651922a481bbbae624c9b2a6c12c0193571e4f73
SamM <s.mcdonald@outlook.com.au>
2025-04-18 12:36:04 +1000

    feat: impl of OpenGLVertexArray

--- src/Runtime/include/NeonRuntime/Backends/OpenGLVertexArray.hpp
+++ src/Runtime/include/NeonRuntime/Backends/OpenGLVertexArray.hpp
@@ -4,14 +4,22 @@
 
 #pragma once
 
+#include <NeonRuntime/Backends/OpenGLHeaders.hpp>
 #include <NeonRuntime/IVertexArray.hpp>
 
 namespace Neon
 {
     class OpenGLVertexArray : public IVertexArray
     {
-        virtual void Bind() const override;
-        virtual void Unbind() const override;
+        public:
+            OpenGLVertexArray();
+            ~OpenGLVertexArray() override;
+
+            void Bind() const override;
+            void Unbind() const override;
+
+        private:
+            GLuint m_vaoId{0};
     };
 }
 
--- src/Runtime/src/Backends/OpenGL/OpenGLVertexArray.cpp
+++ src/Runtime/src/Backends/OpenGL/OpenGLVertexArray.cpp
@@ -6,13 +6,23 @@
 
 namespace Neon
 {
+    OpenGLVertexArray::OpenGLVertexArray()
+    {
+        glGenVertexArrays(1, &m_vaoId);
+    }
+
+    OpenGLVertexArray::~OpenGLVertexArray()
+    {
+        glDeleteVertexArrays(1, &m_vaoId);
+    }
+
     void OpenGLVertexArray::Bind() const
     {
-        // ..
+        glBindVertexArray(m_vaoId);
     }
 
     void OpenGLVertexArray::Unbind() const
     {
-        // ..
+        glBindVertexArray(0);
     }
 }
