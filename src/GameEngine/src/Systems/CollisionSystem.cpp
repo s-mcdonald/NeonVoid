d37ba3cb439d46ee4722c35297e0b5e174da9c4b
SamM <s.mcdonald@outlook.com.au>
2025-04-22 11:24:51 +1000

    feat: Collision Detection - part A

---
+++ src/GameEngine/src/Systems/CollisionSystem.cpp
@@ -0,0 +1,30 @@
+/**
+ *    ███╗   ██╗███████╗ ██████╗ ███╗   ██╗
+ *    ████╗  ██║██╔════╝██╔═══██╗████╗  ██║
+ *    ██╔██╗ ██║█████╗  ██║   ██║██╔██╗ ██║
+ *    ██║╚██╗██║██╔══╝  ██║   ██║██║╚██╗██║
+ *    ██║ ╚████║███████╗╚██████╔╝██║ ╚████║
+ *    ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝  ╚═══╝
+ * ========================================
+ *                 NeonVoid
+ *         A 2D Game Engine in C++
+ * ----------------------------------------
+ * License: MIT
+ * Copyright (c) : 2024 Sam McDonald
+ * Repository: https://github.com/s-mcdonald/NeonVoid
+ */
+
+#include <NeonEngine/CollisionSystem.hpp>
+
+namespace Neon
+{
+    void CollisionSystem::RegisterEntity(Entity* entity)
+    {
+        // ..
+    }
+
+    bool CollisionSystem::AABBCollision(const CollisionComponent& a, const CollisionComponent& b)
+    {
+        return false;
+    }
+}
