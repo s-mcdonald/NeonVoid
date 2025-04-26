f9950e1dff719d65244bb570a0149df2892e808b
SamM <s.mcdonald@outlook.com.au>
2025-04-26 23:05:35 +1000

    style: cleanup old comments

--- src/Runtime/src/Backends/OpenGL/OpenGLVertexBuffer.cpp
+++ src/Runtime/src/Backends/OpenGL/OpenGLVertexBuffer.cpp
@@ -66,10 +66,9 @@ namespace Neon
         glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
         glBufferData(GL_ARRAY_BUFFER, m_size, m_vertices, GL_STATIC_DRAW);
 
-        // Bind and set EBO
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(m_indices), m_indices, GL_STATIC_DRAW);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO); // Bind EBO for quad rendering
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
 
         glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
@@ -93,8 +92,8 @@ namespace Neon
 
     void OpenGLVertexBuffer::UpdateIndices(const void* indices, size_t size)
     {
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO); // Bind the EBO
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
         glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, static_cast<GLsizeiptr>(size), indices);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); // Unbind the EBO
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     }
 }
