af7166cb89d7aee654623dfbbd1f7fd272e10a28
SamM <s.mcdonald@outlook.com.au>
2025-05-03 14:51:46 +1000

    feat: script to remove all existing libs from system

---
+++ scripts/linux_remove_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo "Removing System Shared Libraries"
+echo "------------------------------------"
+echo ""
+echo ""
+#sudo apt remove -y libglew-dev
+#sudo apt remove -y libglew2.2
+#sudo apt remove -y libglfw3
+#sudo apt remove -y libglfw3-dev
+sudo apt remove -y libglm
+sudo apt remove -y libglm-dev
+echo ""
+echo "Completed.."
