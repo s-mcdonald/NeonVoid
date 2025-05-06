137a052a449a4cf7497908cd123d9ac63c1f1749
Sam McDonald <s.mcdonald@outlook.com.au>
2025-05-06 21:06:26 +1000

    windows conversion

--- scripts/windows_build_vs2022.bat
+++ scripts/windows_build_vs2022.bat
@@ -3,8 +3,9 @@
 :: Set the build configuration (Debug or Release)
 set BUILD_TYPE=Debug
 
-cmake -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% .
+cmake -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ..
 
-cmake --build .. --config %BUILD_TYPE%
+@REM cmake --build .. --config %BUILD_TYPE%
+cmake --build .
 
 pause
